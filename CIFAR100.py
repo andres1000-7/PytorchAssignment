ce46753af0baeecd6337aada4941e4c40cec7350
andres1000-7 <96263820+andres1000-7@users.noreply.github.com>
2023-05-29 04:04:21 -0400

    changes

--- CIFAR100.py
+++ CIFAR100.py
@@ -40,43 +40,34 @@ test_loader = DataLoader(test, batch_size=64, num_workers=2)
 class VGG16_NET(nn.Module):
     def __init__(self):
         super(VGG16_NET, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1)
+        self.conv5 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1)
+        self.conv6 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1)
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
-        self.relu1 = nn.ReLU(inplace=True)
-        self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        self.relu2 = nn.ReLU(inplace=True)
-        self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
-        self.relu3 = nn.ReLU(inplace=True)
-        self.maxpool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.fc1 = nn.Linear(256 * 4 * 4, 512)
-        self.relu4 = nn.ReLU(inplace=True)
-
-        self.fc2 = nn.Linear(512, 100)  # Output classes: 100
+        self.flatten = nn.Flatten()
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(in_features=401408, out_features=1024)
+        self.fc2 = nn.Linear(in_features=1024, out_features=100)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.relu1(x)
-        x = self.maxpool1(x)
-
-        x = self.conv2(x)
-        x = self.relu2(x)
-        x = self.maxpool2(x)
-
-        x = self.conv3(x)
-        x = self.relu3(x)
-        x = self.maxpool3(x)
-
-        x = x.view(x.size(0), -1)
-
-        x = self.fc1(x)
-        x = self.relu4(x)
-
-        x = self.fc2(x)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = self.maxpool(x)
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = self.maxpool(x)
+        x = F.dropout(x, 0.25)
+        x = F.relu(self.conv5(x))
+        x = F.relu(self.conv6(x))
+        x = self.maxpool(x)
+        x = F.dropout(x, 0.25)
+        x = self.flatten(x)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, 0.5)
+        x = F.relu(self.fc2(x))
 
         return x
 
